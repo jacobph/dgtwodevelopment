b560b120ac9edf7de62aaf5f5fd5463fe5315d49
Jacob Harris <jacob@outerplaces.com>
2017-02-17 17:45:46 -0500

    start stylin!

--- wp-content/themes/dg/header.php
+++ wp-content/themes/dg/header.php
@@ -44,27 +44,19 @@
 	?>
 >
 
-<header id="masthead" class="site-header">
-	<div class="container center">
-	
-		<nav class="site-navigation main-navigation">
-			<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
-		</nav><!-- .site-navigation .main-navigation -->
-	</div>
-	<div class="center">
+<header class="site-header">
+	<div class="container">
+		<div class="row site-header_contents">
+			<a class="logo" href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home">
+				Donovan/Green
+			</a>
+			<div class="hamburger js_launch-nav"></div>
+			<nav class="site-nav main-nav">
+				<?php wp_nav_menu( array( 'theme_location' => 'primary' ) ); // Display the user-defined menu in Appearance > Menus ?>
+			</nav>
+		</div> <!-- .row.site-header_contents -->
+	</div><!-- .container -->
+</header> <!-- .site-header -->
 
-		<div id="brand">
-			<h1 class="site-title">
-				<a href="<?php echo esc_url( home_url( '/' ) ); // Link to the home page ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); // Title it with the blog name ?>" rel="home"><?php bloginfo( 'name' ); // Display the blog name ?></a>
-			</h1>
-			<h4 class="site-description">
-				<?php bloginfo( 'description' ); // Display the blog description, found in General Settings ?>
-			</h4>
-		</div><!-- /brand -->
-		
-		<div class="clear"></div>
-	</div><!--/container -->
-		
-</header><!-- #masthead .site-header -->
 
 <main class="main-fluid"><!-- start the page containter -->
